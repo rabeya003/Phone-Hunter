72964ef2a637e41b6c1e634aea0a95f22fcdfcc8
rabeya003 <rabeya15-13483@diu.edu.bd>
2023-09-29 15:23:01 +0600

    show all

--- js/app.js
+++ js/app.js
@@ -1,23 +1,33 @@
-const loadPhone=async(search)=>{
+const loadPhone=async(search,dataLimit)=>{
     const url=`https://openapi.programming-hero.com/api/phones?search=${search}`
     fetch(url)
     const res=await fetch(url);
     const data=await res.json();
-    displayPhone(data.data);
+    displayPhone(data.data, dataLimit);
 }
-const displayPhone=phones=>{
+const displayPhone=(phones,dataLimit)=>{
     const phoneContainer=document.getElementById('phone-container');
     phoneContainer.innerHTML=``;
-    phones=phones.slice(0,20)
+    // display 10 phones only
+    const showAll=document.getElementById('showAll');
+      if(dataLimit && phones.length>10){
+          phones=phones.slice(0,10);
+          showAll.classList.remove('d-none')
+      }
+      else{
+        showAll.classList.add('d-none')
+      }
+
   // display no phones found:
     const notFound=document.getElementById('no-found-message');
-    if(phones.length==0){
-      notFound.classList.remove('d-none')
-    }
-    else{
-      notFound.classList.add('d-none')
-    }
-   // display all phones
+        if(phones.length==0){
+          notFound.classList.remove('d-none')
+        }
+        else{
+          notFound.classList.add('d-none')
+        }
+
+// display all phones
     phones.forEach(phone => {
         const div=document.createElement('div')
         div.classList.add('col');
@@ -32,30 +42,44 @@ const displayPhone=phones=>{
         `
       phoneContainer.appendChild(div)
     });
+
 // stop loader
-toggleSpinner(false);
+      toggleSpinner(false);
+      }
 
-
+      
+const processSearch=(dataLimit)=>{
+  toggleSpinner(true);
+  const inputField=document.getElementById('input')
+  const inputFieldValue=inputField.value;
+  loadPhone(inputFieldValue,dataLimit)
+  // inputField.value='';
 }
+
+
+
 // search button click
     const search=()=>{
-    // start loader
-    toggleSpinner(true);
-    const inputField=document.getElementById('input')
-    const inputFieldValue=inputField.value;
-    loadPhone(inputFieldValue)
-    inputField.value='';
+      processSearch(10);
+
 }
 
 
 // loader condition
   const toggleSpinner=isLoading=>{
   const loaderSection=document.getElementById('loader')
-  if(isLoading){
-    loaderSection.classList.remove('d-none')
-  }
-  else{
-    loaderSection.classList.add('d-none')
-  }
+    if(isLoading){
+      loaderSection.classList.remove('d-none')
+    }
+    else{
+      loaderSection.classList.add('d-none')
+    }
 }
+
+
+// there is no data limit->SHOW-ALL
+  document.getElementById('btn-showAll').addEventListener('click',function(){
+    processSearch();
+})
+
 // loadPhone();
